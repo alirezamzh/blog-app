3c210b7f539c78b5540bc74b917c3f4a73236abe
Alireza Memarzade <alireza.am885@gmail.com>
2021-04-08 11:41:33 +0430

    add form into project with update and delete buttons

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -7,4 +9,7 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
     
